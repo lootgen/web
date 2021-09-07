d979a52773f4e04a45df6853858a917a791d7c68
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-06 21:44:11 -0400

    Refactor loot bag into a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,12 @@
-import "./App.css";
+import './App.css';
 
-import React, { useRef, useState } from "react";
-
-const TOTAL_ROWS = 8;
-const MIN_INDEX = 0;
-const MAX_INDEX = TOTAL_ROWS - 1;
-const MAX_CHARACTERS = 45;
+import React from 'react';
+import LootBag from './components/LootBag';
 
 function App(): JSX.Element {
-  const [items, setItems] = useState(["", "", "", "", "", "", "", ""]);
-  const firstEmptyIndex = items.findIndex((value, index) => value.length === 0);
-  const hasEmptyItem = firstEmptyIndex !== -1;
-  const inputRefs = useRef<Array<HTMLInputElement>>([]);
-  const emptyRows = items.reduce((total, item) => {
-    return item.length === 0 ? total + 1 : total;
-  }, 0);
-  const validRows = TOTAL_ROWS - emptyRows;
-
   return (
     <div className="App">
-      <div className="loot-view">
-        <div
-          className={validRows === 0 ? "item-counter empty" : "item-counter"}
-        >
-          {`${validRows}/${MAX_INDEX + 1}`}
-        </div>
-        <div className="loot-items">
-          {items.map((item, index) => (
-            <input
-              key={`loot-item-${index}`}
-              maxLength={MAX_CHARACTERS}
-              spellCheck={false}
-              autoCapitalize="on"
-              ref={(ref) => {
-                if (ref) {
-                  inputRefs.current[index] = ref;
-                }
-              }}
-              value={item}
-              placeholder={index === firstEmptyIndex ? "Add Loot..." : ""}
-              onKeyDown={({ key }) => {
-                if (key === "ArrowDown" || key.includes("Enter")) {
-                  if (items[index].length > 0 && index < MAX_INDEX) {
-                    inputRefs.current[index + 1].focus();
-                  }
-                }
-                if (key === "ArrowUp") {
-                  if (index > MIN_INDEX) {
-                    inputRefs.current[index - 1].focus();
-                  }
-                }
-              }}
-              onFocus={(event) => {
-                if (hasEmptyItem && index > firstEmptyIndex) {
-                  inputRefs.current[firstEmptyIndex].focus();
-                }
-              }}
-              onChange={({ target }) => {
-                if (items[index].length === 0 && target.value.trim().length === 0) {
-                  return;
-                }
-                const newItems = [...items];
-                newItems[index] = target.value;
-                setItems(newItems);
-              }}
-              onBlur={({ target }) => {
-                const newItems = [...items];
-                newItems[index] = target.value.trim();
-                setItems(newItems);
-              }}
-            />
-          ))}
-        </div>
-        {emptyRows < TOTAL_ROWS && (
-          <div
-            className={
-              hasEmptyItem ? "submit-button disabled" : "submit-button"
-            }
-            onClick={() => {}}
-          >
-            Generate
-          </div>
-        )}
-      </div>
+      <LootBag />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
+import './index.css';
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const SERVER_URL = "http://MacBook-ESQ.local:4000";
+
+const APOLLO_CLIENT = new ApolloClient({
+  uri: SERVER_URL,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={APOLLO_CLIENT}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
