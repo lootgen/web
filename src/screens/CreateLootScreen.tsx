7ab7e5294138954e17aea4f5edfb229ac77b0668
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-19 21:57:34 -0400

    Add initial loot examples on home page

--- src/screens/CreateLootScreen.tsx
+++ src/screens/CreateLootScreen.tsx
@@ -1,7 +1,10 @@
+import './CreateLootScreen.css';
+
 import { useContext } from 'react';
 
 import { DimBackgroundContext } from '../App';
 import LootBag from '../components/LootBag';
+import { lootBags } from './LootData';
 
 const CreateLootScreen: React.FC = () => {
   const dimBackground = useContext(DimBackgroundContext);
@@ -13,6 +16,19 @@ const CreateLootScreen: React.FC = () => {
           dimBackground();
         }}
       />
+
+      <div className="more-lootgen">
+        <p>More LootGen:</p>
+        {lootBags.map((lootBag, index) => (
+          <LootBag
+            key={`loot-${index}`}
+            itemNames={lootBag}
+            locked
+            hideShareButtons
+            itemsOnly
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/screens/LootData.ts
@@ -0,0 +1,91 @@
+export const lootBags = [
+  [
+    'Person',
+    'Woman',
+    'Man',
+    'Camera',
+    'TV',
+    '“Extra points”',
+    'Difficult +1',
+    'Cognitive test',
+  ],
+
+  [
+    'Whoa, whoa, whoa',
+    'There’s still plenty of meat on that bone',
+    'Now you take this home',
+    'Throw it in a pot',
+    'Add some broth',
+    'A potato',
+    'Baby',
+    'You got a stew going',
+  ],
+
+  [
+    'Brown couch',
+    'Floor lamp',
+    '“Sailboat painting”',
+    'Side table with lamp and phone',
+    'Record player',
+    'Pink, purple, cyan rug',
+    'TV + antenna',
+    'Doorway to kitchen',
+  ],
+
+  [
+    'Hello, American investor',
+    'I see you are interested in distributing',
+    'Mr. Sparkle in your home prefecture',
+    'You have chosen wisely',
+    "But, please, don't believe me",
+    'Observe this commercial',
+    'Awesome power!',
+    "There's your answer fish-bulb",
+  ],
+
+  [
+    'Flip on the telly',
+    'Wrestle with Jimmy',
+    'Something is bubbling',
+    'Behind my back',
+    'The bottle is ready to blow',
+    "Say it ain't so",
+    'Your drug is a heart breaker',
+    "Say it ain't so",
+  ],
+
+  ['☕️', '☕️', '☕️', '☕️', '☕️', '☕️', '☕️', 'gm'],
+
+  [
+    'Breakfast:',
+    'Eggs',
+    'Bacon',
+    'Toast',
+    'Pancakes',
+    'Maple syrup',
+    'Orange Juice',
+    'Coffee',
+  ],
+
+  [
+    'Peanut Butter',
+    'Jelly Time',
+    'Peanut Butter',
+    'Jelly Time',
+    'Peanut Butter Jelly',
+    'Peanut Butter Jelly',
+    'Peanut Butter Jelly',
+    'And a Baseball Bat',
+  ],
+
+  [
+    'Gimme the loot',
+    'Gimme the loot',
+    "I'm a bad, bad, bad",
+    'Gimme the loot',
+    'Gimme the loot',
+    "I'm a bad, bad, bad",
+    "Big up big up it's a stick up stick up",
+    "And I'm shootin' n***** if ya hiccup",
+  ],
+];
